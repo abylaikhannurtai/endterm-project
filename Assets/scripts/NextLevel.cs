a5457706af772cae1d5dc4bfae0b39e74d5b44e9
an0ri0n <devilsmile53@gmail.com>
2021-12-14 05:18:59 +0600

    added simple UI | ver 0.4

--- Assets/scripts/NextLevel.cs
+++ Assets/scripts/NextLevel.cs
@@ -3,20 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextLevel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
+public class NextLevel : MonoBehaviour {    
     void OnCollisionEnter(Collision col) {{
     	Debug.Log("Collision Detected It Works");
     	SceneManager.LoadScene("Level 2");
